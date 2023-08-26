1aadf598874f15ad068cda6122dae5ec5050f63f
nikita-t1 <26936278+nikita-t1@users.noreply.github.com>
2023-08-26 15:19:15 +0200

    refactor: embed markdown file

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ package main
 
 import (
 	"context"
+	_ "embed"
 	"errors"
 	"fmt"
 	"github.com/charmbracelet/bubbles/help"
@@ -32,66 +33,8 @@ const (
 
 var helpStyle = lipgloss.NewStyle().Foreground(lipgloss.Color("241")).Render
 
-const content = `
-# ssh.nikitatarasov.dev
-
-## Moin
-
-I'm Nikita Tarasov, a self-taught developer from Germany
-
-with experience in Full-Stack Development using Java and JavaScript. 
-
- - Let's get in touch -> [hi@nikitatarasov.dev]()
-
-## Background
-
- Hi!
-
-I'm a Software Developer and Electronics Technician based in Germany
-
-During the final year of my training as an Electronics Technician for Devices and Systems,
-I was introduced to modern software development. 
-I created a "Cocktail Machine" using Java on a Raspberry Pi 4, controlling pumps via GPIO pins,
-guided by an experienced developer who offered insights and best practices.
-
-After completing my training, I started working as a Test Field Technician. 
-However, a portion of my time I contributed to improving and further developing the company's internal error database. 
-This project involved a JavaEE server, a Spring MVC web interface, and a JavaFX Client.
-
-After switching to a different company, I didn't have the opportunity to work on any software projects 
-so I decided to start learning on my own. During free time, I started learning web development. 
-I started with HTML, CSS, and JavaScript, and later I moved on to Vue.js. 
-I also started learning Kotlin for Backend and Android development and Python for small scripts. 
-
-## About Me
-
-Alongside my development work, I also have a solid foundation in electronics and hardware.
-I love tinkering with electronics and building small projects using microcontrollers like the Arduino and microcomputers like the Raspberry Pi.
-
-I also enjoy watching movies and TV shows in my free time. 
-My favorite genres are science fiction and superhero movies. You can follow my watch progress on [Trakt](https://trakt.tv/users/nikita-t1).
-
-Also I'm really into reading books. 
-I mostly read non-fiction books about social sciences, psychology, and politics, but occasionally I also read some science fiction or even romance novels. 
-You can follow my reading progress on [Goodreads](https://www.goodreads.com/user/show/143627750-nikita).
-
-
-## Skills
-
-| Languages  | Frameworks            | Tools         |
-|------------|-----------------------|---------------|
-| JavaScript | Vue                   | VS Code       |
-| TypeScript | Node.js               | WebStorm      |
-| HTML       | Nuxt                  | Gradle        |
-| CSS        | Prisma                | Maven         |
-| Java       | Spring Boot           | IntelliJ IDEA |
-| Kotlin     | Ktor                  | Eclipse IDE   |
-| Python     | Compose Multiplatform | Git           |
-| SQL        | JavaFX                | Bash          |
-|            | Hibernate             | Docker        |
-
-# Bon appétit!
-`
+//go:embed websiteContent.md
+var websiteContent string
 
 func middlewareWithLogger() wish.Middleware {
 	return func(sh ssh.Handler) ssh.Handler {
@@ -188,7 +131,7 @@ func initModel(m model) (model, error) {
 		return m, nil
 	}
 
-	str, err := renderer.Render(content)
+	str, err := renderer.Render(websiteContent)
 	if err != nil {
 		return m, nil
 	}
---
+++ websiteContent.md
@@ -0,0 +1,59 @@
+
+# ssh.nikitatarasov.dev
+
+## Moin
+
+I'm Nikita Tarasov, a self-taught developer from Germany
+
+with experience in Full-Stack Development using Java and JavaScript.
+
+- Let's get in touch -> [hi@nikitatarasov.dev]()
+
+## Background
+
+Hi!
+
+I'm a Software Developer and Electronics Technician based in Germany
+
+During the final year of my training as an Electronics Technician for Devices and Systems,
+I was introduced to modern software development.
+I created a "Cocktail Machine" using Java on a Raspberry Pi 4, controlling pumps via GPIO pins,
+guided by an experienced developer who offered insights and best practices.
+
+After completing my training, I started working as a Test Field Technician.
+However, a portion of my time I contributed to improving and further developing the company's internal error database.
+This project involved a JavaEE server, a Spring MVC web interface, and a JavaFX Client.
+
+After switching to a different company, I didn't have the opportunity to work on any software projects
+so I decided to start learning on my own. During free time, I started learning web development.
+I started with HTML, CSS, and JavaScript, and later I moved on to Vue.js.
+I also started learning Kotlin for Backend and Android development and Python for small scripts.
+
+## About Me
+
+Alongside my development work, I also have a solid foundation in electronics and hardware.
+I love tinkering with electronics and building small projects using microcontrollers like the Arduino and microcomputers like the Raspberry Pi.
+
+I also enjoy watching movies and TV shows in my free time.
+My favorite genres are science fiction and superhero movies. You can follow my watch progress on [Trakt](https://trakt.tv/users/nikita-t1).
+
+Also I'm really into reading books.
+I mostly read non-fiction books about social sciences, psychology, and politics, but occasionally I also read some science fiction or even romance novels.
+You can follow my reading progress on [Goodreads](https://www.goodreads.com/user/show/143627750-nikita).
+
+
+## Skills
+
+| Languages  | Frameworks            | Tools         |
+|------------|-----------------------|---------------|
+| JavaScript | Vue                   | VS Code       |
+| TypeScript | Node.js               | WebStorm      |
+| HTML       | Nuxt                  | Gradle        |
+| CSS        | Prisma                | Maven         |
+| Java       | Spring Boot           | IntelliJ IDEA |
+| Kotlin     | Ktor                  | Eclipse IDE   |
+| Python     | Compose Multiplatform | Git           |
+| SQL        | JavaFX                | Bash          |
+|            | Hibernate             | Docker        |
+
+# Bon appétit!
