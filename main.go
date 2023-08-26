d73ed6b028cff8719959cbec521b72b770a69387
nikita-t1 <26936278+nikita-t1@users.noreply.github.com>
2023-08-26 15:02:49 +0200

    feat: add help view

--- main.go
+++ main.go
@@ -7,6 +7,8 @@ import (
 	"context"
 	"errors"
 	"fmt"
+	"github.com/charmbracelet/bubbles/help"
+	"github.com/charmbracelet/bubbles/key"
 	"github.com/charmbracelet/glamour"
 	"os"
 	"os/signal"
@@ -160,7 +162,9 @@ func myCustomBubbleteaMiddleware() wish.Middleware {
 			width:    pty.Window.Width,
 			height:   pty.Window.Height,
 			time:     time.Now(),
-			viewport: viewport.New(pty.Window.Width, pty.Window.Height),
+			viewport: viewport.New(pty.Window.Width, pty.Window.Height-4),
+			help:     help.New(),
+			keys:     keys,
 		}
 		return newProg(m, tea.WithInput(s), tea.WithOutput(s), tea.WithAltScreen())
 	}
@@ -199,6 +203,66 @@ type model struct {
 	height   int
 	time     time.Time
 	viewport viewport.Model
+	help     help.Model
+	keys     keyMap
+}
+
+// keyMap defines a set of keybindings. To work for help it must satisfy
+// key.Map. It could also very easily be a map[string]key.Binding.
+type keyMap struct {
+	Up    key.Binding
+	Down  key.Binding
+	Left  key.Binding
+	Right key.Binding
+	View  key.Binding
+	Help  key.Binding
+	Quit  key.Binding
+}
+
+// ShortHelp returns keybindings to be shown in the mini help view. It's part
+// of the key.Map interface.
+func (k keyMap) ShortHelp() []key.Binding {
+	return []key.Binding{k.Help, k.View, k.Quit}
+}
+
+// FullHelp returns keybindings for the expanded help view. It's part of the
+// key.Map interface.
+func (k keyMap) FullHelp() [][]key.Binding {
+	return [][]key.Binding{
+		{k.Up, k.Down, k.Left, k.Right}, // first column
+		{k.Help, k.View, k.Quit},        // second column
+	}
+}
+
+var keys = keyMap{
+	Up: key.NewBinding(
+		key.WithKeys("up", "k"),
+		key.WithHelp("↑/k", "move up"),
+	),
+	Down: key.NewBinding(
+		key.WithKeys("down", "j"),
+		key.WithHelp("↓/j", "move down"),
+	),
+	Left: key.NewBinding(
+		key.WithKeys("left", "h"),
+		key.WithHelp("←/h", "move left"),
+	),
+	Right: key.NewBinding(
+		key.WithKeys("right", "l"),
+		key.WithHelp("→/l", "move right"),
+	),
+	View: key.NewBinding(
+		key.WithKeys("v"),
+		key.WithHelp("v", "toggle view"),
+	),
+	Help: key.NewBinding(
+		key.WithKeys("?", "/"),
+		key.WithHelp("?", "toggle help"),
+	),
+	Quit: key.NewBinding(
+		key.WithKeys("q", "esc", "ctrl+c"),
+		key.WithHelp("q", "quit"),
+	),
 }
 
 type timeMsg time.Time
@@ -212,13 +276,15 @@ func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 	case timeMsg:
 		m.time = time.Time(msg)
 	case tea.WindowSizeMsg:
-		m.height = msg.Height
+		m.height = msg.Height - 4
 		m.width = msg.Width
 		m, _ = initModel(m)
 	case tea.KeyMsg:
-		switch msg.String() {
-		case "q", "ctrl+c":
+		switch {
+		case key.Matches(msg, m.keys.Quit):
 			return m, tea.Quit
+		case key.Matches(msg, m.keys.Help):
+			m.help.ShowAll = !m.help.ShowAll
 		default:
 			var cmd tea.Cmd
 			m.viewport, cmd = m.viewport.Update(msg)
@@ -238,6 +304,7 @@ func (m model) View() string {
 		s = "Terminal too small to display\n"
 		return fmt.Sprintf(s)
 	}
-	return m.viewport.View()
+	help := lipgloss.NewStyle().PaddingLeft(2).Render(helpStyle(m.help.View(m.keys)))
+	return m.viewport.View() + "\n" + help
 	//return fmt.Sprintf(s, m.term, m.width, m.height)
 }
